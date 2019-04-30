b5d7bbaef0f0d4f7b1c71c02c0b95c24b4d43240
Ethan Brierley <ethanboxx@gmail.com>
2019-04-30 10:32:48 +0100

    basic layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
 #![deny(clippy::pedantic)]
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
+
+use std::{
+    iter::Iterator,
+    path::Path,
+    rc::Rc,
+    thread,
+    time::{self, SystemTime},
+};
+
+struct FileListBuilder {
+    files: Vec<WatchedFile>,
+}
+
+struct WatchedFile {
+    path: String,
+    time: SystemTime,
+    function_on_run: Rc<Fn() -> ()>,
+}
+
+impl FileListBuilder {
+    fn launch(self) -> () {}
+}
