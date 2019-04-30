a8348203d506016e599ae7c6eb83d0ce6707c187
Ethan Brierley <ethanboxx@gmail.com>
2019-04-30 14:43:25 +0100

    WatchedFile

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 // #![warn(missing_docs)]
 
 use std::{
+    path::Path,
     rc::Rc,
     time::{Duration, SystemTime},
 };
@@ -9,12 +10,18 @@ use std::{
 struct FileListBuilder {
     files: Vec<WatchedFile>,
     interval: Duration,
+    max_retries: Option<u32>,
 }
 
 struct WatchedFile {
     path: String,
     time: SystemTime,
-    function_on_run: Rc<Fn() -> ()>,
+    functions_on_run: Vec<Rc<Fn(String) -> WatchingFuncResult>>,
+}
+
+enum WatchingFuncResult {
+    Success,
+    Retry,
 }
 
 impl FileListBuilder {
@@ -22,7 +29,37 @@ impl FileListBuilder {
         Self {
             files: Vec::new(),
             interval: Duration::from_millis(1000),
+            max_retries: None,
         }
     }
     fn launch(self) -> () {}
+    fn add_file(mut self, file: WatchedFile) -> Self {
+        self.files.push(file);
+        self
+    }
+    fn with_interval(mut self, inter: Duration) -> Self {
+        self.interval = inter;
+        self
+    }
+    fn with_max_retries(mut self, re: u32) -> Self {
+        self.max_retries = Some(re);
+        self
+    }
+}
+
+impl WatchedFile {
+    fn new(path: String) -> Result<Self, String> {
+        Ok(Self {
+            path: path,
+            time: Path::new(&path)
+                .metadata()
+                .set_error(&format!("failed to open file {} metadata", path.display()))?
+                .modified()
+                .set_error(&format!(
+                    "failed to find files date modifide {}",
+                    path.display()
+                )),
+            functions_on_run: Vec::new(),
+        })
+    }
 }
